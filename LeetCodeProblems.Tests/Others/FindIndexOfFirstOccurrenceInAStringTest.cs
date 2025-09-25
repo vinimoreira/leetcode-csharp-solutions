c907bd9e73bf6ce2a82fbf2e88b28c844acb2f56
Vinicius Moreira <vini.moreira13@hotmail.com>
2025-09-25 13:44:01 -0300

    feature: Include Edge Cases Tests

--- LeetCodeProblems.Tests/Others/FindIndexOfFirstOccurrenceInAStringTest.cs
+++ LeetCodeProblems.Tests/Others/FindIndexOfFirstOccurrenceInAStringTest.cs
@@ -10,24 +10,32 @@ public class FindIndexOfFirstOccurrenceInAStringTest
     [Theory]
     [InlineData("sadbutsad", "sad", 0)]
     [InlineData("leetcode", "leeto", -1)]
-    public void GetLengthOfLastWord_ExampleCases_ReturnsExpectedResult(string input, string find, int expected)
+    public void FindIndexOfFirstOccurrenceInAString_ExampleCases_ReturnsExpectedResult(string input, string find, int expected)
     {
         var result = _solution.FindIndexOfFirstOccurrence(input, find);
         Assert.Equal(expected, result);
     }
 
-    // [Theory]
-    // [InlineData(null, 0)]
-    // [InlineData("", 0)]
-    // [InlineData("   ", 0)]
-    // [InlineData("a", 1)]
-    // [InlineData(" a ", 1)]
-    // public void GetLengthOfLastWord_EdgeCases_ReturnsExpectedResult(string input, int expected)
-    // {
-    //     // Act
-    //     var result = _solution.GetLengthOfLastWord(input);
+    [Theory]
+    [InlineData(null, "a", -1)]
+    [InlineData("a", null, -1)]
+    [InlineData("", "", -1)]
+    [InlineData("abc", "", -1)]
+    [InlineData("", "a", -1)]
+    [InlineData("a", "aa", -1)]
+    [InlineData("aaaaa", "bba", -1)]
+    [InlineData("mississippi", "issi", 1)]
+    [InlineData("aaa", "aa", 0)]
+    [InlineData("ababa", "aba", 0)]
+    [InlineData("needle", "needle", 0)]
+    [InlineData("endswith", "with", 4)]
+    [InlineData("CaseSensitive", "casesensitive", -1)]
+    public void FindIndexOfFirstOccurrenceInAString_EdgeCases_ReturnsExpectedResult(string input, string find, int expected)
+    {
+        // Act
+        var result = _solution.FindIndexOfFirstOccurrence(input, find);
 
-    //     // Assert
-    //     Assert.Equal(expected, result);
-    // }
+        // Assert
+        Assert.Equal(expected, result);
+    }
 }
--- LeetCodeProblems/Others/FindIndexOfFirstOccurrenceInAString.cs
+++ LeetCodeProblems/Others/FindIndexOfFirstOccurrenceInAString.cs
@@ -8,13 +8,21 @@ public class FindIndexOfFirstOccurrenceInAString
     // Example 1:
     // Input: haystack = "sadbutsad", needle = "sad"
     // Output: 0
+    // I oppted to not use the String IndexOf
     public int FindIndexOfFirstOccurrence(string haystack, string needle)
     {
         if (string.IsNullOrEmpty(haystack) || string.IsNullOrEmpty(needle))
             return -1;
 
+        if(needle.Length > haystack.Length)
+            return -1;
+
         for (int i = 0; i <= haystack.Length; i++)
         {
+            //Prevent ArgumentOutOfRangeException
+            if (haystack.Length - i < needle.Length)
+                return -1;
+
             var subString = haystack.Substring(i, needle.Length);
             if (string.IsNullOrEmpty(subString) || subString != needle)
                 continue;
